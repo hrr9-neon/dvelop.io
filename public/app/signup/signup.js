24cdc416f86c3bcc80277b14ee11fe0e6d20a6d9
rascle <ryan.bascle@gmail.com>
2015-12-02 17:19:25 -0600

    Fixed basic functionality. Authentication and routing. Search users.

--- public/app/signup/signup.js
+++ public/app/signup/signup.js
@@ -3,13 +3,10 @@ angular.module('dvelop.signup', ['dvelop.auth'])
 .controller('SignupController', function(logout, currentAuth, $scope, Auth, $location, $rootScope, $firebaseObject){
 
   $scope.user = {};
-  $scope.currentUser;
 
   var userData = $rootScope.fb.child('users/' + $rootScope.loggedIn.userID);
 
   userData.on('value', function(snapshot){
-    console.log(snapshot.val());
-    console.log(snapshot.key());
     $scope.currentUser = (snapshot.val());
     $scope.user.displayName = $scope.currentUser.displayName;
     $scope.user.emailAddress = $scope.currentUser.emailAddress;
@@ -21,14 +18,12 @@ angular.module('dvelop.signup', ['dvelop.auth'])
     $scope.user.helpAvail = $scope.currentUser.helpAvail;
     $scope.user.job = $scope.currentUser.job;
     $scope.user.mentorAvail = $scope.currentUser.mentorAvail;
-
-    // $scope.dataHasLoaded = true;
   });
 
   $scope.saveData = function(){
     var userRef = $rootScope.fb.child('users');
     userRef.child($rootScope.loggedIn.userID).update($scope.user);
-    $location.path('/search'); //object version
+    // $location.path('/search'); //object version
   };
 
   $scope.logout = logout.logout;
