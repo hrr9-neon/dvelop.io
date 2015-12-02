e413ff0f59bf64371a705dc1d684e3280b51f661
Ahmet Cetin <ahmet@cetin.info>
2015-12-03 00:50:40 +0100

    Messages beginning

--- public/app/messages/messages.js
+++ public/app/messages/messages.js
@@ -1,64 +1,93 @@
-angular.module('dvelop.messages', ['dvelop.auth'])
-.factory('rooms', function($firebase, $rootscope) {
+angular.module('dvelop.messages', [])
+
+.factory('MyRooms', function($rootScope, $firebase) {
+  var myRooms = $rootScope.fb.child('users/' + $rootScope.loggedIn.userID + '/rooms');
+
+  var getRooms = function(callback) {
+    myRooms.on('value', function(snap) {
+      var roomlist = [];
+      var result = snap.val();
+
+      for (var key in result) {
+        if (result[key] !== 'private') {
+          roomlist.push({
+            id: key,
+            name: result[key]
+          });
+        }
+      }
+
+      callback(roomlist);
+    });
+  };
+
+  return {
+    getRooms: getRooms
+  };
 
 })
 
-.controller('MessagesController', function ($scope, logout, $location, $firebaseArray, $firebaseObject, $rootScope){
+.factory('MyMessages', function($rootScope, $firebase) {
 
-
-  $scope.publicrooms = [];
-  $scope.privaterooms = [];
-
-  var myRoomList = [];
-
-  var myRooms = new Firebase("https://shining-torch-3159.firebaseio.com/users/" + $rootScope.loggedIn.userID + '/rooms');
-
-  var checkRooms = function() {
-    myRooms.on('child_added', function(snapshot){
-      console.log(snapshot.val());
-      console.log(snapshot.key());
-
-      var roomID = snapshot.key();
-
-      var roomInfo = new Firebase("https://shining-torch-3159.firebaseio.com/rooms/" + roomID);
-
-      roomInfo.once('value', function(item) {
-        var roomDetail = item.val();
-        console.log(roomDetail);
-        if (roomDetail.type === 'public') {
-          $scope.privaterooms.push({
-            id: roomID,
-            name: roomDetail.name
-          });
-        }
-      });
-    });
-
+  var getMessages = function(roomID, callback) {
+    var myMessages = $firebaseArray($rootScope.fb.child('messages/' + roomID));
+    console.log(myMessages);
+    callback(myMessages);
   };
 
 
-  checkRooms();
-  // // List the names of all Mary's groups
-  // var ref = new Firebase("https://shining-torch-3159.firebaseio.com");
+  var getMessages = function(roomID, callback) {
+    var myMessages = $rootScope.fb.child('messages/' + roomID);
 
-  // // fetch a list of Mary's groups
-  // ref.child("users/" + $rootScope.loggedIn.userID + "/rooms").on('child_added', function(snapshot) {
-  //   // for each group, fetch the name and print it
-  //   var roomKey = snapshot.key();
-  //   ref.child("rooms/" + roomKey + "/name").once('value', function(snapshot) {
-  //     System.out.println("Mary is a member of this group: " + snapshot.val());
-  //   });
-  // });
+    myMessages.on('value', function(snap) {
+      var msglist = [];
+      var result = snap.val();
+      console.log(result);
+
+      for (var key in result) {
+        if (result[key] !== 'private') {
+          msglist.push({
+            id: key,
+            text: result[key].text,
+            sender: result[key].sender,
+            timestamp: result[key].timestamp
+          });
+        }
+      }
+
+      callback(msglist);
+    });
+  };
+
+  return {
+    getMessages: getMessages
+  };
+})
+
+
+// .factory('UserNames', function($rootScope, $firebase) {
+
+// })
+
+.controller('MessagesController', function ($scope, $rootScope, logout, MyRooms, MyMessages){
+  $scope.messages;
+  $scope.publicrooms;
+
+  MyRooms.getRooms(function(list) {
+    $scope.publicrooms = list;
+    console.log($scope.publicrooms);
+  });
+
+  $scope.showMessages = function(room) {
+    console.log(room.name);
+    MyMessages.getMessages(room.id, function(msg) {
+      $scope.messages = msg;
+      console.log(msg[0].text);
+    });
+  };
 
 
 
-  // var myRooms = $firebaseArray(new Firebase("https://shining-torch-3159.firebaseio.com/users/" + $rootScope.loggedIn.userID + '/rooms'));
-
-
-
-  // console.log(data.publicrooms);
-
-  //logout func
   $scope.logout = logout.logout;
 
 });
