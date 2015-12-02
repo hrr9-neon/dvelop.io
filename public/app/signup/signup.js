a85f6d32776af4d1548732a098d40f8b8394681e
Ahmet Cetin <ahmet@cetin.info>
2015-12-02 03:54:33 +0100

    Update signup.js

--- public/app/signup/signup.js
+++ public/app/signup/signup.js
@@ -1,34 +1,31 @@
 angular.module('dvelop.signup', ['dvelop.auth'])
 
-.controller('SignupController', function(currentAuth, $scope, UserStore, Auth, $location){
-  
+.controller('SignupController', function(currentAuth, $scope, Auth, $location, $rootScope, $firebaseObject){
+
   $scope.user = {};
   $scope.currentUser;
 
-  //Populate form new user's Github data
-  Auth.$onAuth(function(authData){
-    $scope.authData = authData;
-    $scope.currentUser = UserStore[$scope.authData.github.id];
+  var userData = new Firebase("https://shining-torch-3159.firebaseio.com/users/" + $rootScope.loggedIn.userID);
 
-    var name = $scope.currentUser.displayName;
-
+  userData.on('value', function(snapshot){
+    console.log(snapshot.val());
+    $scope.currentUser = (snapshot.val());
     $scope.user.displayName = $scope.currentUser.displayName;
     $scope.user.emailAddress = $scope.currentUser.email;
-    $scope.user.birthday = "";
-    $scope.user.professionalLevel = "";
-    $scope.user.profileImageUrl = $scope.currentUser.imageURL;
-    $scope.user.address = '';
-    $scope.user.bestAt = '';
-    $scope.user.helpAvail = true;
-    $scope.user.job = '';
+    // $scope.user.birthday = $scope.currentUser.birthday;
+    $scope.user.professionalLevel = $scope.currentUser.prefessionalLevel;
+    $scope.user.profileImageUrl = $scope.currentUser.profileImageUrl;
+    $scope.user.address = $scope.currentUser.address;
+    $scope.user.bestAt = $scope.currentUser.bestAt;
+    $scope.user.helpAvail = $scope.currentUser.helpAvail;
+    $scope.user.job = $scope.currentUser.job;
+    $scope.user.mentorAvail = $scope.currentUser.mentorAvail;
+  });
 
-  $scope.user.mentorAvail = true;
-  })
-
-$scope.saveData = function(){
-	var userRef = new Firebase("https://shining-torch-3159.firebaseio.com/users"); 
-	userRef.child($scope.authData.github.id).update($scope.user);
-	$location.path('/search'); //object version
-}
+  $scope.saveData = function(){
+    var userRef = new Firebase("https://shining-torch-3159.firebaseio.com/users");
+    userRef.child($scope.authData.github.id).update($scope.user);
+    $location.path('/search'); //object version
+  };
 
 });
